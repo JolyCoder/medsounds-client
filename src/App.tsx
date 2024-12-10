8ec723bcd4de11383a957d4a00fc9d5a86c40a61
Ilya Tvaliashvili <itvaliaschvili@icloud.com>
2024-12-11 01:05:48 +0300

    added app boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router";
+
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.pageContainer}>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
+import { PATHS } from "./consts";
+import "./index.css";
+import App from "./App.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: PATHS.HOME,
+        element: null,
+      },
+      {
+        path: PATHS.NEWS,
+        element: null,
+      },
+      {
+        path: PATHS.ABOUT,
+        element: null,
+      },
+      {
+        path: "*",
+        element: <Navigate to={PATHS.HOME} />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
