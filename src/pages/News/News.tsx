a75732f45a58a6ef9a78534b5bcc6eecf45f7131
Ilya Tvaliashvili <itvaliaschvili@icloud.com>
2024-12-12 22:44:20 +0300

    updated news component

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -1,11 +1,3 @@
 import type { FC } from "react";
-import { useQuery } from "react-query";
-import { postsApi } from "../../api";
 
-export const News: FC = () => {
-  const { isLoading, data } = useQuery(...postsApi.getPosts);
-
-  console.log(data);
-
-  return null;
-};
+export const News: FC = () => null;
