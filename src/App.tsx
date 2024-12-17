3089c883acba7c2777811a4804e5de6e86ece430
Ilya Tvaliashvili <itvaliaschvili@icloud.com>
2024-12-18 01:02:11 +0300

    fixed bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 
 import styles from "./App.module.css";
 import { Header } from "./components/Header/Header";
 import { AppSuspense } from "./components/AppSuspense/AppSuspense";
+import { PATHS } from "./consts";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!Object.values(PATHS).includes(location.pathname)) {
+      navigate("/home");
+    }
+  }, [location.pathname, navigate]);
+
   return (
     <div className={styles.container}>
       <Header />
