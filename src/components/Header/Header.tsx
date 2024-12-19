f609524633e189ca1417de4f60854763b1d93a41
Ilya Tvaliashvili <itvaliaschvili@icloud.com>
2024-12-19 23:37:46 +0300

    added onClick for logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react";
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import cn from "classnames";
 
 import styles from "./Header.module.css";
@@ -8,10 +8,16 @@ import { PATHS } from "../../consts";
 import logo from "../../assets/logo.png";
 
 export const Header: FC = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate(PATHS.HOME);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.block}>
-        <img className={styles.logo} src={logo} />
+        <img className={styles.logo} src={logo} onClick={handleLogoClick} />
       </div>
 
       <div className={styles.block}>
